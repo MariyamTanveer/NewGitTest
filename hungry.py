ba1437e68d5f51eaf51ef2ad00b361fd504adc6b
MariyamTanveer <mariyam.tanveer99@gmail.com>
2020-04-27 23:53:45 +0500

    water

--- hungry.py
+++ hungry.py
@@ -2,4 +2,8 @@ hungry = input("are you hungry?")
 if hungry == "yes":
     print("idc")
 else:
-    print("ok")
+    thirsty = input("are u thirsty")
+    if thirsty == "yes":
+        print("okkk")
+    else:
+        print("dfbdfjb")
